5ce882f8f5818b6eb7264dc8306ae49ad1f89099
Maksim Sergienko <creativesun@yandex.ru>
2026-08-08 11:26:28 +0300

    feat(App): integrate CurrentGameView and GamesHistoryView components into the main application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
 import { AppFooter } from './components/app-footer'
 import { AppHeader } from './components/app-header'
+import { CurrentGameView } from './components/current-game-view'
+import { GamesHistoryView } from './components/games-history-view'
 
 function App() {
 
   return (
     <>
       <AppHeader />
-      Hello there!
+      <div>
+        <CurrentGameView />
+      </div>
+      <div>
+        <GamesHistoryView />
+      </div>
       <AppFooter />
     </>
   )
